9fdf7b671213b4eb5390ab7437c6b17d7c750494
Turtle <turtleletortuelab@gmail.com>
2020-09-28 10:52:39 -0400

    st18c10bnn: Fix messed up file path & add changes from other mt8167 devices

--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@
 TARGET_PREBUILT_KERNEL := device/bn/st18c10bnn/kernel
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/init.recovery.mt8167.rc:root/init.recovery.mt8163.rc \
+    $(LOCAL_PATH)/recovery/init.recovery.mt8167.rc:root/init.recovery.mt8167.rc \
     $(LOCAL_PATH)/recovery/init.mt8167.usb.rc:root/init.mt8167.usb.rc 
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
